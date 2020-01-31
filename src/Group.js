02470eff3fb3ac10e360b130ef14e2fe9b71ed39
Gregory Arnheiter <gmarnheiter@gmail.com>
2020-01-31 13:38:11 -0800

    Created Group Class

---
+++ src/Group.js
@@ -0,0 +1,60 @@
+const noomman = require('noomman');
+const ClassModel = noomman.ClassModel;
+const Channelable = require('./Channelable');
+
+const Group = new ClassModel({
+    className : 'Group',
+    superClasses : [Channelable],
+    attributes : [
+        {
+            name : 'name',
+            type : String,
+            required : true,
+        },
+        {
+            name : 'description',
+            type : String,
+            required : false,
+        }
+    ],
+    relationships : [
+        {
+            name : 'positionDefinitions',
+            toClass : 'PositionDefinition',
+            singular : false,
+            required : false,
+            mirrorRelationship : true
+        },
+        {
+            name : 'positions',
+            toClass : 'Position',
+            singular : false,
+            required : false,
+            mirrorRelationship : true
+        },
+        {
+            name : 'motions',
+            toClass : 'Motion',
+            singular : false,
+            required : false,
+            mirrorRelationship : true
+        },
+        {
+            name : 'subGroups',
+            toClass : 'Group',
+            singular : false,
+            required : false,
+            mirrorRelationship : true
+        },
+        {
+            name : 'superGroup',
+            toClass : 'Group',
+            singular : true,
+            required : false,
+            mirrorRelationship : true
+        }
+    ]
+  
+});
+
+module.exports = Group;
