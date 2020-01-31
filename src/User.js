de1c304a2a34702c162cd0866a495345d346d6bb
Gregory Arnheiter <gmarnheiter@gmail.com>
2020-01-31 12:25:31 -0800

    Updated User Class for new relationships

--- src/User.js
+++ src/User.js
@@ -26,6 +26,36 @@ const User = new ClassModel({
             toClass : 'Account',
             singular : true,
             required : true
+        },
+        {
+            name : 'event',
+            toClass : 'Event',
+            singular : false,
+            required : false
+        },
+        {
+            name : 'appointment',
+            toClass : 'Appointment',
+            singular : true,
+            required : false
+        },
+        {
+            name : 'position',
+            toClass : 'Position',
+            singular : false,
+            required : false
+        },
+        {
+            name : 'message',
+            toClass : 'Message',
+            singular : false,
+            required : false
+        },
+        {
+            name : 'messageBoard',
+            toClass : 'MessageBoard',
+            singular : false,
+            required : false
         }
     ]
   
