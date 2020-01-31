a464d9af43dc8a7a257034f53e4ff268c3266978
Gregory Arnheiter <gmarnheiter@gmail.com>
2020-01-31 12:40:57 -0800

    Created Position Class

---
+++ src/Position.js
@@ -0,0 +1,48 @@
+const noomman = require('noomman');
+const ClassModel = noomman.ClassModel;
+
+const Position = new ClassModel({
+    className : 'Position',
+    
+    relationships : [
+        {
+            name : 'positionDefinition',
+            toClass : 'PositionDefinition',
+            singular : true,
+            required : true
+        },
+        {
+            name : 'group',
+            toClass : 'Group',
+            singular : true,
+            required : true
+        },
+        {
+            name : 'user',
+            toClass : 'User',
+            singular : true,
+            required : true
+        },
+        {
+            name : 'individualVote',
+            toClass : 'IndividualVote',
+            singular : false,
+            required : false
+        },
+        {
+            name : 'appointment',
+            toClass : 'Appointment',
+            singular : false,
+            required : false
+        },
+        {
+            name : 'motion',
+            toClass : 'Motion',
+            singular : false,
+            required : false
+        }
+    ]
+  
+});
+
+module.exports = PositionDescription;
