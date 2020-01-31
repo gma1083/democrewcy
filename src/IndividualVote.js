3378095fac773f909b60393b30973db94d2fa280
Gregory Arnheiter <gmarnheiter@gmail.com>
2020-01-31 12:43:49 -0800

    Created Individual Vote Class

---
+++ src/IndividualVote.js
@@ -0,0 +1,31 @@
+const noomman = require('noomman');
+const ClassModel = noomman.ClassModel;
+
+const IndividualVote = new ClassModel({
+    className : 'IndividualVote',
+    
+    relationships : [
+        {
+            name : 'position',
+            toClass : 'Position',
+            singular : true,
+            required : true
+        },
+        {
+            name : 'vote',
+            toClass : 'Vote',
+            singular : true,
+            required : true
+        },
+        {
+            name : 'voteOption',
+            toClass : 'voteOption',
+            singular : true,
+            required : true
+        },
+        
+    ]
+  
+});
+
+module.exports = PositionDescription;
